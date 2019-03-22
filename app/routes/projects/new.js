5157f934bd83fdd9f43f45a5b054d1fb6a55bec2
kevin <brissetkevin9@gmail.com>
2019-03-22 16:35:10 +0100

    commit

--- app/routes/projects/new.js
+++ app/routes/projects/new.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(){
-		return {copy:{}};
+		return {copy:{}, owner: this.store.findAll('developer')};
 	},
 
 	actions:{
